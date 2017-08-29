514bbd3d1b7cafc9dd7af8439f9874cbd2304729
James <jfmatthews90@gmail.com>
2017-08-29 15:54:36 -0400

    Added validations to post method, updated new controller, and added error messages to new.html.erb file.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,17 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create # allows the cration of a new post with a set of params that can be saved
 		@post = Post.new(post_params)
-		@post.save
 
-		redirect_to @post
+		if @post.save # allows you to be redirected to a validated post, otherwise it will render the new form
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show # allows you to show a post based on its id, which is the /1, /2, etc... in a url
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+	# below validates that content exists in a post before allowing it to be submitted
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :body, presence: true
 end
